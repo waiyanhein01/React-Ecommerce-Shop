fcce6dd2c3046ea064f73a3339def91bd1f7087b
waiyanhein01 <wyan00196@gmail.com>
2024-09-05 02:21:30 +0630

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./route/Router.route.jsx";
+
 // import "atropos/scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router}/>);
